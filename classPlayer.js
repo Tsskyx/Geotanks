194473e103983a4bdcbc9847880598fb3bb34e00
Tsskyx <tom.stehlik@gmail.com>
2023-08-11 23:59:11 +0200

    Create classPlayer.js

---
+++ classPlayer.js
@@ -0,0 +1,13 @@
+class Player {
+    constructor() {
+        this.speedMultiplier = 5;
+        this.pos = [0, 0];
+        this.vel = [0, 0];
+        this.acc = [0, 0];
+        this.move = [0, 0, 0, 0];
+    }
+
+    draw() {
+
+    }
+}
